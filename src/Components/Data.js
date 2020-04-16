bff4f85177e1e9e1e09498a96c987df79adbe024
Hakan Gezer <hakangezer@Hakans-MBP.fritz.box>
2020-04-16 15:37:43 +0200

    superdigital-update2

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,30 +1,42 @@
 export const contact = [
   {
     id: 0,
-    titel: "Kostenloser HTML & CSS Workshop für Einsteiger:innen",
+    titel: "SuperDigital eV – Wir starten ab April!",
     datum: "15.April 2020",
     beschreibung:
       "Bleib nicht stumm! Lerne deine ersten Sätze in HTML und CSS(...)",
+    image: "./people.png",
+    beschreibungLang:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
   },
   {
     id: 1,
-    titel: "Kostenloser HTML & CSS Workshop für Einsteiger:innen",
+    titel: "Bitkomm Studie zum Fachkräftemangel 2020",
     datum: "21.April 2020",
     beschreibung:
       "Bleib nicht stumm! Lerne deine ersten Sätze in HTML und CSS(...)",
+    image: "./Bild5.png",
+    beschreibungLang:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
   },
   {
     id: 2,
-    titel: "Fe:male Hackaton by FemaleInnovation Hub",
+    titel: "Better Place – Digitale Wege der Integration",
     datum: "26.April 2020",
     beschreibung:
       "Wir sind dabei - du auch? Fe:male Hackaton from Fe:male Innovation Hub@Sipgate(...)",
+    image: "./Bild6.png",
+    beschreibungLang:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
   },
   {
     id: 3,
-    titel: "Kongress: Die Zukunft der digitalen Bildung",
+    titel: "Agile goes Non-Profit",
     datum: "28.April 2020",
     beschreibung:
       "Welche Bildung brauchen wir und in was wollen wir uns bilden? Ein Kongress, gemeinsam mit(...)",
+    image: "./Bild4.png",
+    beschreibungLang:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
   },
 ];

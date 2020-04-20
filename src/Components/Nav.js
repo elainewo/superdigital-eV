f1cd03f7321bdff0731e8b67c5d54bfa8996b46a
Hakan Gezer <hakangezer@Hakans-MBP.fritz.box>
2020-04-20 09:57:52 +0200

    superdigital-update12

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,7 +5,7 @@ import { FaArrowLeft } from "react-icons/fa";
 import Modal from "react-modal";
 Modal.setAppElement("#root");
 
-const Nav = () => {
+const Nav = (props) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div>
@@ -16,18 +16,18 @@ const Nav = () => {
 
         <nav className="navigation-items">
           <ul>
-            <li>Home</li>
-            <li>Projekte</li>
-            <li>Über Mich</li>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#beitraege">Projekte</a>
+            </li>
+            <li>
+              <a href="#uebermich">Über Mich</a>
+            </li>
             <button className="modal" onClick={() => setModalIsOpen(true)}>
               <li className="kontakt">Kontakt</li>
             </button>
-            {/* <li><a href="#home">Home</a>
-            </li>
-            <li><a href="#beitraege">Projekte</a>
-            </li>
-            <li><a href="#uebermich">Über Mich</a></li>
-            <li className="kontakt">Kontakt</li> */}
           </ul>
         </nav>
         <Modal
@@ -52,39 +52,47 @@ const Nav = () => {
             erreichen.
           </p>
           <h3 className="kontact">Kontakt</h3>
-          <p>
-            <label className="daten">Name</label>
-            <input
-              className="input"
-              type="name"
-              name=""
-              id=""
-              placeholder="Vor- und Nachname"
-            />
-          </p>
-          <p>
-            <label className="daten">Email Adress</label>
-            <input
-              className="input"
-              type="email"
-              name=""
-              id=""
-              placeholder="Hier dein Email"
-            />
-          </p>
-          <p>
-            <label className="daten">Nachricht</label>
-            <input
-              className="input"
-              type="text"
-              name=""
-              id=""
-              placeholder="Schreib uns deine Nachricht"
-            />
-          </p>
-          <div className="submit">
-            <button className="cool-button4">Senden</button>
-          </div>
+          <form onSubmit={props.handleSubmit} name="contact">
+            <p>
+              <label className="daten">Name</label>
+              <input
+                value={props.name}
+                onChange={props.handleChange}
+                className="input"
+                type="name"
+                name="name"
+                id=""
+                placeholder="Vor- und Nachname"
+              />
+            </p>
+            <p>
+              <label className="daten">Email Adress</label>
+              <input
+                value={props.email}
+                onChange={props.handleChange}
+                className="input"
+                type="email"
+                name="email"
+                id=""
+                placeholder="Hier dein Email"
+              />
+            </p>
+            <p>
+              <label className="daten">Nachricht</label>
+              <input
+                value={props.message}
+                onChange={props.handleChange}
+                className="input"
+                type="text"
+                name="message"
+                id=""
+                placeholder="Schreib uns deine Nachricht"
+              />
+            </p>
+            <div className="submit">
+              <button className="cool-button4">Senden</button>
+            </div>
+          </form>
           <div>
             <button className="close" onClick={() => setModalIsOpen(false)}>
               <FaArrowLeft size="2rem" />
